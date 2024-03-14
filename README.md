72afd7051ae0953094f0994bd8bd95a89a3dabef
rodgersgb <greg.b.rodgers@gmail.com>
2024-03-14 12:27:00 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# stackblitz-starters-ha6lw8
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/rodgersgb/stackblitz-starters-ha6lw8)
---
+++ src/app/user.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@angular/core';
+import { map, Observable, of } from 'rxjs';
+
+export interface User {
+  id: string;
+  username: string;
+  email: string;
+  firstName: string;
+  lastName: string;
+  password: string;
+}
+
+@Injectable({ providedIn: 'root' })
+export class UserService {
+  constructor() {}
+
+  getUsers(): Observable<User[]> {
+    return of(USERS).pipe(map((response) => response as User[]));
+  }
+
+  authenticate(userNameOrEmail: string, password: string): Observable<boolean> {
+    throw new Error('not implemented');
+  }
+}
+
+const USERS = [
+  {
+    id: '001',
+    username: 'user001',
+    email: 'user001@mail.com',
+    firstName: 'User',
+    lastName: '001',
+    password: '001',
+  },
+  {
+    id: '002',
+    username: 'user002',
+    email: 'user002@mail.com',
+    firstName: 'User',
+    lastName: '002',
+    password: '002',
+  },
+  {
+    id: '003',
+    username: 'user003',
+    email: 'user003@mail.com',
+    firstName: 'User',
+    lastName: '003',
+    password: '003',
+  },
+];
